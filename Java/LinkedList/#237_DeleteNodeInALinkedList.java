742d0a2a91d04dd219d77231ac11aeca7790694d
LiuJiawei4132 <45940407+LiuJiawei4132@users.noreply.github.com>
2019-11-29 16:44:54 +0800

    Create #237_DeleteNodeInALinkedList.java

---
+++ Java/LinkedList/#237_DeleteNodeInALinkedList.java
@@ -0,0 +1,18 @@
+/**
+ * 因为node不会是最后一个节点，所以我们可以对节点的值进行操作。
+ */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
