95e43cd7938edeb69f296386132bfe76781b0245
LiuJiawei4132 <1018928466@qq.com>
2019-12-06 16:35:45 +0800

    2019.12.6 first submission

---
+++ Java/LinkedList/#21_MergeTwoSortedListsk.java
@@ -0,0 +1,28 @@
+/**
+ * 定义一个哑节点，比较两个链表的值，串起来就可
+ */
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        // maintain an unchanging reference to node ahead of the return node.
+        ListNode prehead = new ListNode(-1);
+
+        ListNode prev = prehead;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                prev.next = l1;
+                l1 = l1.next;
+            } else {
+                prev.next = l2;
+                l2 = l2.next;
+            }
+            prev = prev.next;
+        }
+
+        // exactly one of l1 and l2 can be non-null at this point, so connect
+        // the non-null list to the end of the merged list.
+        prev.next = l1 == null ? l2 : l1;
+
+        return prehead.next;
+    }
+}
---
+++ Java/LinkedList/#445_AddTwoNumbersII.java
@@ -0,0 +1,46 @@
+/* 
+    
+*/
+
+class Solution {
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+         Stack<Integer> stack1 = new Stack<>();
+         Stack<Integer> stack2 = new Stack<>();
+         while (l1 != null) {
+             stack1.push(l1.val);
+             l1 = l1.next;
+         }
+         while (l2 != null) {
+             stack2.push(l2.val);
+             l2 = l2.next;
+         }
+         // 进位
+         int carry = 0;
+         ListNode dummy = new ListNode(0);
+         ListNode temp = dummy;
+         while (stack1.size() > 0 || stack2.size() > 0) {
+             int n1 = stack1.size() == 0 ? 0 : stack1.pop();
+             int n2 = stack2.size() == 0 ? 0 : stack2.pop();
+             int sum = n1 + n2 + carry;
+             carry = sum / 10;
+             dummy.next = new ListNode(sum % 10);
+             dummy = dummy.next;
+         }
+         // 如果最后进位了
+         if (carry > 0) {
+             dummy.next = new ListNode(carry);
+         }
+         return reverse(temp.next);
+     }
+ 
+     // 翻转链表
+     private static ListNode reverse(ListNode head) {
+         if (head == null || head.next == null) {
+             return head;
+         }
+         ListNode last = reverse(head.next);
+         head.next.next = head;
+         head.next = null;
+         return last;
+     }
+ }
